3d3f8b8ce5ec5bc5e850059de17743b5b336ed72
3Mad <ysf1026@gmail.com>
2023-01-06 23:35:46 +0800

    协程并发量控制器

---
+++ go/refactor/generic/pool.go
@@ -0,0 +1,39 @@
+package generic
+
+import (
+	"sync"
+)
+
+type Pool struct {
+	queue chan int
+	wg    *sync.WaitGroup
+}
+
+func NewPool(conCnt, total int) Pool {
+	if conCnt < 1 {
+		conCnt = 1
+	}
+
+	if total < conCnt {
+		conCnt = total
+	}
+
+	return Pool{
+		queue: make(chan int, conCnt),
+		wg:    new(sync.WaitGroup),
+	}
+}
+
+func (m *Pool) AddOne() {
+	m.wg.Add(1)
+	m.queue <- 1
+}
+
+func (m *Pool) DelOne() {
+	<-m.queue
+	m.wg.Done()
+}
+
+func (m *Pool) Waiting() {
+	m.wg.Wait()
+}
