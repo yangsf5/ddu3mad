7032f956f32f31c4dc1a7f82162242fc480653fe
3Mad <ysf1026@gmail.com>
2023-01-12 18:33:18 +0800

    append slice 时，第一个参数如果是带类型的 nil 是可以的；否则也不行

--- go/slice/slice_test.go
+++ go/slice/slice_test.go
@@ -12,3 +12,9 @@ func TestRangeWrite(t *testing.T) {
 	ret2 := RangeWriteMap()
 	fmt.Printf("%v\n", ret2)
 }
+
+func TestAppend(t *testing.T) {
+	// s := append(nil, []string{}) // 不行
+	s := append([]string(nil), "test")
+	fmt.Printf("%v\n", s)
+}
