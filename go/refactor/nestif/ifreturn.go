7a70cf610746ae8137a250cc720188fde867c317
3Mad <ysf1026@gmail.com>
2023-02-07 00:31:29 +0800

    利用 if return 来减少 if 复杂度和缩进层次

---
+++ go/refactor/nestif/ifreturn.go
@@ -0,0 +1,39 @@
+package ifreturn
+
+func BadFunc(a, b, c, d int) int {
+	if a != 1 {
+		if b != 2 {
+			if c != 3 {
+				if d != 4 {
+					// do something
+					// ...
+					return 1
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
+func Refactor(a, b, c, d int) int {
+	if a == 1 {
+		return 0
+	}
+
+	if b == 2 {
+		return 0
+	}
+
+	if c == 3 {
+		return 0
+	}
+
+	if d == 4 {
+		return 0
+	}
+
+	// do something
+	// ...
+	return 1
+}
