6551ecda83408c3d8d4ffb9dca18a533e3569dba
3Mad <ysf1026@gmail.com>
2023-02-07 00:48:24 +0800

    条件反过来，才比较常见；要体现多行

--- go/refactor/nestif/ifreturn.go
+++ go/refactor/nestif/ifreturn.go
@@ -1,13 +1,19 @@
-package ifreturn
+package nestif
 
-func BadFunc(a, b, c, d int, s []string) int {
-	if a != 1 {
-		if b != 2 {
-			if c != 3 {
-				if d != 4 {
+func BadManyLevel(a, b, c, d int, s []string) int {
+	if a == 1 {
+		// do something: a
+		if b == 2 {
+			// do something: b
+			if c == 3 {
+				// do something: c
+				if d == 4 {
+					// do something: d
 					for range s {
 						// do something
-						// ...
+						// many lines ...
+						// many lines ...
+						// many lines ...
 					}
 
 					return 1
@@ -19,26 +25,36 @@ func BadFunc(a, b, c, d int, s []string) int {
 	return 0
 }
 
-func Refactor(a, b, c, d int, s []string) int {
-	if a == 1 {
+func RefactorManyLevel(a, b, c, d int, s []string) int {
+	if a != 1 {
 		return 0
 	}
 
-	if b == 2 {
+	// do something: a
+
+	if b != 2 {
 		return 0
 	}
 
-	if c == 3 {
+	// do something: b
+
+	if c != 3 {
 		return 0
 	}
 
-	if d == 4 {
+	// do something: c
+
+	if d != 4 {
 		return 0
 	}
+
+	// do something: d
 
 	for range s {
 		// do something
-		// ...
+		// many lines ...
+		// many lines ...
+		// many lines ...
 	}
 
 	return 1
