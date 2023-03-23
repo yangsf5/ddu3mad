b8f1b14e0cdc7b07f198060ca5a073c73e24e8eb
3Mad <ysf1026@gmail.com>
2023-03-23 16:19:38 +0800

    TabPages

--- demo/plasmo-demo/src/popup.tsx
+++ demo/plasmo-demo/src/popup.tsx
@@ -24,7 +24,27 @@ function IndexPopup() {
         <input onChange={(e) => setData(e.target.value)} value={data} />
         <br/>
         <Button type="primary">点我应该没反应才对</Button>
-
+        <br/>
+        <button onClick={() => {
+          chrome.tabs.create({
+            url: './tabs/delta-flyer.html'
+          })
+        }}>open tab page</button>
+        <br/>
+        <button onClick={() => {
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const { id } = tabs[0]
+            chrome.scripting.executeScript({
+              target: { tabId: id },
+              func: () => {
+                const iframe = document.createElement("iframe")
+                iframe.src = chrome.runtime.getURL("/tabs/delta-flyer.html")
+                iframe.name = "delta-flyer"
+                document.body.appendChild(iframe)
+              }
+            })
+          })
+        }}>iframe mounting</button>
       </div>
     </ThemeProvider>
   )
