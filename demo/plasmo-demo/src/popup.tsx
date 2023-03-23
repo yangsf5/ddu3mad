3fcea21e0d39efe183b307a693a4b7911be000b4
3Mad <ysf1026@gmail.com>
2023-03-23 15:53:44 +0800

    引入 antd

--- demo/plasmo-demo/src/popup.tsx
+++ demo/plasmo-demo/src/popup.tsx
@@ -1,27 +1,32 @@
+import Button from "antd/es/button"
 import { useState } from "react"
+
+import { ThemeProvider } from "~theme"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <ThemeProvider>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 16
+        }}>
+        <h2>
+          Welcome to your{" "}
+          <a href="https://www.plasmo.com" target="_blank">
+            Plasmo
+          </a>{" "}
+          Extension!
+        </h2>
+        <input onChange={(e) => setData(e.target.value)} value={data} />
+        <br/>
+        <Button type="primary">点我应该没反应才对</Button>
+
+      </div>
+    </ThemeProvider>
   )
 }
 
